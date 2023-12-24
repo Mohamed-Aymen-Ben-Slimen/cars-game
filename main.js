a00e58ab8a9facf8130ffb4f010cc6707fd2290b
MohamedAymenBenSlimen <mohamed.aymen.ben.slimen@gmail.com>
2023-12-24 20:22:14 +0100

    fix cars position

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ const ctx = canvas.getContext('2d');
 
 const road = new Road({ x: canvas.width / 2, width: canvas.width, laneCount: 3 });
 
-const car = new Car({ x: road.getLineCenter(1), y: 100, width: 30, height: 50, controlActive: true });
+const car = new Car({ x: road.getLineCenter(1), y: 0, width: 30, height: 50, controlActive: true });
 
 const traffic = [
-    new Car({ x: road.getLineCenter(0), y: 60, width: 30, height: 50, maxSpeed: 2, color: "blue" }),
-    new Car({ x: road.getLineCenter(1), y: 30, width: 30, height: 50, maxSpeed: 2.2, color: "blue" }),
-    new Car({ x: road.getLineCenter(2), y: 20, width: 30, height: 50, maxSpeed: 2.4, color: "blue" }),
-
+    new Car({ x: road.getLineCenter(0), y: -60, width: 30, height: 50, maxSpeed: 2, color: "blue" }),
+    new Car({ x: road.getLineCenter(1), y: -80, width: 30, height: 50, maxSpeed: 2.2, color: "blue" }),
+    new Car({ x: road.getLineCenter(2), y: -70, width: 30, height: 50, maxSpeed: 2.4, color: "blue" }),
+    new Car({ x: road.getLineCenter(0), y: -120, width: 30, height: 50, maxSpeed: 2, color: "blue" }),
+    new Car({ x: road.getLineCenter(1), y: -90, width: 30, height: 50, maxSpeed: 2, color: "blue" }),
+    new Car({ x: road.getLineCenter(2), y: -190, width: 30, height: 50, maxSpeed: 2.1, color: "blue" }),
 ]
 
 function animate() {
@@ -25,7 +27,7 @@ function animate() {
     canvas.height = window.innerHeight;
 
     ctx.save();
-    ctx.translate(0, -car.y + canvas.height / 2);
+    ctx.translate(0, -car.y + canvas.height - 100);
 
     road.draw(ctx);
 
